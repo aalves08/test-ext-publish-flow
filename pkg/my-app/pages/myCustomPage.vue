97fbca21fbff52405d4dd1a9b48a88b6c6404480
Alexandre Alves <alexandre.alves@suse.com>
2025-08-22 09:56:49 +0100

    add test extension

---
+++ pkg/my-app/pages/myCustomPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>HELLO WORLD</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SomeComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
